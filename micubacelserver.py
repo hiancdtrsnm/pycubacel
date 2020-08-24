b450397a49894445aaf31e6a603a91226ff00322
fsadannn <fsadannn@gmail.com>
2020-08-23 21:51:13 -0400

    rewrite extraction process

---
+++ micubacelserver.py
@@ -0,0 +1,16 @@
+from micubacel import consult_get
+from flask import Flask, jsonify
+from pathlib import Path
+
+# openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout key.pem -days 365
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+def hello():
+    data = consult_get(Path('./config.json'))
+    return jsonify(data)
+
+if __name__ == "__main__":
+	#app.run(host='127.0.0.1', debug=True, ssl_context=('cert.pem', 'key.pem'))
+    app.run(host='127.0.0.1', debug=True)
